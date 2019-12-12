2147e08e585f4afd01b0d081d0f03fd18c6dcbdd
Adam Rae <262674+raemond@users.noreply.github.com>
2019-12-12 16:32:16 +0000

    Add script for converting csv to json, updated README

--- convertCSVtoJSON.py
+++ convertCSVtoJSON.py
@@ -1,45 +1,58 @@
 import csv
 import json
 
+def mk_int(s):
+    s = s.strip()
+    return int(s) if s else 0
+
+def mk_float(s):
+    s = s.strip()
+    return float(s) if s else 0
+
 csvfile = open('summaries.csv', 'r')
-jsonfile = open('summaries.json', 'w')
 
-fieldnames = ("first","second","third")
-#reader = csv.DictReader(csvfile, fieldnames)
-# for row in reader:
-# 	json.dump(row, jsonfile)
-# 	jsonfile.write('\n')
-
+reader = csv.DictReader(csvfile)
 data = []
+counter = 1
+for row in reader:
+	assessment = {}
+	assessment['application'] = {}
+	assessment['application'] = {
+	"application.name": row['application.name'],
+	"application.identifier": row['application.identifier'],
+	"application.planningAuthorityID": row['application.planningAuthorityID']
+	}
+	assessment['property'] = {}
+	assessment['property'] = {
+	"property.addressPrimary": row['property.addressPrimary'],
+	"property.centroidLatLng": row['property.centroidLatLng'],
+	#"property.boundaryPolygon": row['property.boundaryPolygon']
+	}
+	assessment['proposal'] = []
+	assessment['proposal'] = {
+	"proposal.residentialUnits": mk_int(row['proposal.residentialUnits']), 
+	"proposal.habitableRooms": mk_int(row['proposal.habitableRooms']), 
+	"proposal.commercialArea": mk_float(row['proposal.commercialArea']), 
+	"proposal.storeysMax": mk_int(row['proposal.storeysMax']), 
+	"proposal.viabilityAssessmentScenarioDate": row['proposal.viabilityAssessmentScenarioDate'],
+	"proposal.grossDevelopmentValue": mk_int(row['proposal.grossDevelopmentValue']),
+	"proposal.constructionCostsTotal": mk_int(row['proposal.constructionCostsTotal']),
+	"proposal.professionalFeesTotal": mk_int(row['proposal.professionalFeesTotal']),
+	"proposal.marketingAndLettingFeesTotal": mk_int(row['proposal.marketingAndLettingFeesTotal']),
+	"proposal.financeCostTotal": mk_int(row['proposal.financeCostTotal']),
+	"proposal.planningObligationsCostTotal": mk_int(row['proposal.planningObligationsCostTotal']),
+	"proposal.developerProfitTotal": mk_int(row['proposal.developerProfitTotal']),
+	"proposal.residualLandValue": mk_int(row['proposal.residualLandValue']),
+	"proposal.benchmarkLandValue": mk_int(row['proposal.benchmarkLandValue']),
+	"proposal.totalCosts": mk_int(row['proposal.totalCosts']),
+	"proposal.viabilityAssessmentAuthor": row['proposal.viabilityAssessmentAuthor'],
+	"proposal.viabilityAssessmentOnBehalfOf": row['proposal.viabilityAssessmentOnBehalfOf'],
+	"proposal.viabilityScenarioName": row['proposal.viabilityScenarioName']
+	}
+	data.append(assessment)
+	counter += 1
 
-assessment = {}
-assessment['application'] = {}
-assessment['application'] = { "application.name": "a", "application.identifier": "ID"}
-assessment['property'] = {}
-assessment['property'] = { "property.addressPrimary": "b","property.centroidLatLng": "c"}
-assessment['proposal'] = []
-assessment['proposal'] = {"proposal.residentialUnits": 1}
+print("Finished processing",counter,"lines.")
 
-data.append(assessment)
-
-assessment = {}
-assessment['application'] = {}
-assessment['application'] = { "application.name": "1", "application.identifier": "ID"}
-assessment['property'] = {}
-assessment['property'] = { "property.addressPrimary": "2","property.centroidLatLng": "3"}
-assessment['proposal'] = []
-assessment['proposal'] = {"proposal.residentialUnits": 4}
-
-data.append(assessment)
-
-
-#data['applications'].append({'thing': 'a', 'thing2': 'b'})
-
-print(json.dumps(data, indent=4))
-
-with open('summaries.json.txt', 'w') as outfile:
-    json.dump(data, outfile)
-
-#import pandas as pd
-#csv_file = pd.DataFrame(pd.read_csv("summaries.csv", sep = ",", header = 0, index_col = False))
-#csv_file.to_json("summaries.json", orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
+with open('summaries.json', 'w') as outfile:
+    json.dump(data, outfile, indent="\t")
